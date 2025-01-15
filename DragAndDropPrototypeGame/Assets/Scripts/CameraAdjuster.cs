712449b649644d90233e7bdd3cb0e0e13a270008
DeDMaKSiM1 <maxlex2012@gmail.com>
2025-01-15 05:24:43 +0300

    Beginning of setting scene

---
+++ DragAndDropPrototypeGame/Assets/Scripts/CameraAdjuster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraAdjuster : MonoBehaviour
+{
+    private SpriteRenderer templateSprite; 
+    private Camera Camera;
+
+    //private void Start()
+    //{
+
+    //    AdjustCameraToBackground();
+    //}
+
+    [ContextMenu("Set camera's size")]
+    public void AdjustCameraToBackground()
+    {
+        Camera = Camera.main;
+        templateSprite = Resources.Load<GameObject>("TemplateSprite").GetComponent<SpriteRenderer>();
+
+        float spriteHeight = templateSprite.bounds.size.y;
+        Camera.orthographicSize = spriteHeight / 2;
+         
+        Camera.transform.position = new Vector3(templateSprite.transform.position.x,
+                    templateSprite.transform.position.y, Camera.transform.position.z); 
+    }
+}
--- DragAndDropPrototypeGame/Assets/Scripts/DragHandler.cs
+++ DragAndDropPrototypeGame/Assets/Scripts/DragHandler.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class DragHandler : MonoBehaviour
 {
     private Rigidbody2D _rbody;
